7c18e87a354ef5f21a4d8f8637ae4079256b9b06
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2024-03-09 02:14:41 +0100

    Cuda: suppress warning.

--- cuda/reduction.cu
+++ cuda/reduction.cu
@@ -12,6 +12,9 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma nv_diag_suppress 815 // suppress consr return warning
+
 #include <argparser.hpp>
 #include <iostream>
 #include <numeric>
