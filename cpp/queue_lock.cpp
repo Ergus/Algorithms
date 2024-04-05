4b37426213726322ba222ca23a1230c8e64a25e3
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2024-04-05 04:21:36 +0200

    C++: Add queue lock code with a test.
    Also add tests.

---
+++ cpp/queue_lock.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2024  Jimmy Aguilar Mena
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <utils.h>
+
+#include <argparser.hpp>
+#include "queue_lock.hpp"
+
+#include <iostream>
+#include <vector>
+
+// Example usage
+template <size_t MAX, typename T = int>
+struct ProtectedData {
+	QueueLock qLock;
+	T sharedData;
+
+	std::vector<int> ids { std::vector<int>(MAX) }; // TODO
+	std::atomic<size_t> it {0};
+	size_t count {0};
+
+public:
+	void Write(int id) {
+		// There is not any guarantee that the threads are actually created with
+		// equivalent latency. So they will arrive here in arbitrary order.
+		// This code is actually simple.
+		// We atomically store the id order of arriving before the lock
+		// (unprotected, but atomically). And then we check that the same order
+		// is respected in the lock take (inside the lock).
+		ids[it++] = id;
+
+		qLock.Lock();
+		T readed = sharedData;
+		sharedData = id;
+		std::cout << "Writer " << id
+		          << " readed " << readed
+		          << " and wrote : " << sharedData << std::endl;
+
+		if (count > 0)
+			myassert(readed == ids[count++]);
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		qLock.Unlock();
+	}
+
+	operator T() const
+	{
+		return sharedData;
+	}
+};
+
+int main(int argc, char **argv) {
+
+	argparser::init(argc, argv);
+	const size_t nWriters = argparser::cl<size_t>("Writers");
+
+	// The track vector has a limit that needs to be specified and we cannot
+	// exceed.
+	myassert(nWriters < 100);
+	ProtectedData<100> data;
+
+    std::vector<std::thread> writers;
+
+    for (int i = 0; i < nWriters; ++i) {
+		writers.emplace_back(&ProtectedData<100>::Write, &data, i);
+    }
+
+	std::cout << "All threads created" << std::endl;
+
+    for (std::thread& writer : writers)
+        writer.join();
+
+	std::cout << "Final value: " << data << std::endl;
+
+    return 0;
+}
---
+++ hpp/queue_lock.hpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2024  Jimmy Aguilar Mena
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <atomic>
+#include <thread>
+#include <cassert>
+#include <stdexcept>
+
+template<int I>
+class QueueLock_t {
+private:
+
+	class helper {
+
+		friend class QueueLock_t;
+		QueueLock_t* _mutex{nullptr};
+        std::atomic<helper*> _next{nullptr};
+        std::atomic<bool> _status{false};
+
+		void lock(QueueLock_t *m) {
+			_mutex = m;
+            _next.store(nullptr, std::memory_order_relaxed);
+            _status.store(0U, std::memory_order_relaxed);
+
+            // x86 compare exchange operation always has a strong fence
+            // "sending" the fields initialized above to other processors.
+            if (helper* pred = m->_last.exchange(this); pred != nullptr) {
+                if (pred->_next.exchange(this, std::memory_order_release) != nullptr)
+					throw std::runtime_error("There was another successor");
+
+				while (!_status)
+					std::this_thread::yield();
+            }
+        }
+
+		void unlock()
+        {
+			assert(_mutex != nullptr);
+
+			// If there is not _next, then there are two possibilities.
+			// 1. This is the last thread in the queue
+			// 2. There is a race condition because there is a value in the
+			// queue not set in _next yet
+            if (_next.load(std::memory_order_relaxed) == nullptr) {
+
+                if (helper* expected = this;
+				    _mutex->_last.compare_exchange_strong(expected, nullptr)) {
+                    // this was the only item in the queue, and the queue is now empty.
+					_mutex = nullptr;
+                    return;
+                }
+                // If we are here is because there is some entry in the queue
+                // that is not this (compare_exchange return false)
+				// But we need to help because that value is not set (yet) into 
+				// _next
+				while (_next == nullptr)
+					std::this_thread::yield();
+            }
+
+            _next.load(std::memory_order_acquire)->_status.store(true, std::memory_order_release);
+			_mutex = nullptr;
+        }
+	};
+
+	// Negative value indicates a write lock, positive indicates the number of read locks.
+    std::atomic<helper*> _last{nullptr};
+
+	static thread_local helper h;
+
+public:
+	void Lock()
+	{
+		h.lock(this);
+	}
+
+	void Unlock()
+	{
+		h.unlock();
+	}
+};
+
+template<int I>
+thread_local typename QueueLock_t<I>::helper QueueLock_t<I>::h;
+
+using QueueLock = QueueLock_t<0>;
