2ab71b39eb206309f37109209e62babe485c2b52
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2024-09-10 00:07:45 +0200

    C++: Add new operator() to matrix class

--- cpp/matrix.cpp
+++ cpp/matrix.cpp
@@ -19,15 +19,26 @@ int main()
 {
 
 
+	// Construct a zero filled matrix
 	matrix<double> mat(5, 4);
 
+	// Initialize usng the [] operator
 	for (size_t r = 0, count = 0; r < 5; ++r)
 		for (size_t c = 0; c < 4; ++c)
 			mat[r][c] = (double) count++;
 
 
+	// Print the matrix using the overloaded operator<<
 	std::cout << mat << std::endl;
 
+	// Initialize usng the [] operator
+	for (size_t r = 0, count = 0; r < 5; ++r) {
+		for (size_t c = 0; c < 4; ++c) {
+			double tmp = (double) count++;
+			assert(mat[r][c] == tmp);
+			assert(mat(r, c) == tmp);
+		}
+	}
 
 	return 0;
 }
--- hpp/matrix.hpp
+++ hpp/matrix.hpp
@@ -20,12 +20,29 @@ class matrix {
 
 public:
 
-	matrix(size_t rows, size_t cols)
+	class iterator {
+		matrix &_owner;
+		size_t _idx;
+
+		std::span<T> operator*()
+		{
+			return _owner[_idx];
+		}
+	};
+
+	/** Construct zero filled matrix.
+		@params[in] rows Number of rows
+		@params[in] cols Number of columns
+	*/
+	matrix(size_t rows = 0, size_t cols = 0)
 	: _rows(rows), _cols(cols), _mat(rows * cols)
 	{
 	}
 
-
+	/** Row access operator.
+		@params[in] idx the row index to return
+		@return an std::span of the row
+	*/
 	std::span<T> operator[](size_t idx)
 	{
 		if (idx < _rows)
@@ -34,7 +51,10 @@ public:
 		return std::span<T>();
 	}
 
-
+	/** Row access const operator.
+		@params[in] idx the row index to return
+		@return a const std::span of the row
+	*/
 	std::span<const T> operator[](size_t idx) const
 	{
 		if (idx < _rows)
@@ -43,7 +63,28 @@ public:
 		return std::span<const T>();
 	}
 
+	/** Use alternative operator to access elements
+		@params[in] row row element to access
+		@params[in] col column element to access
+		@return element value to return
+	*/
+	T operator()(size_t row, size_t col)
+	{
+		assert(row < _rows);
+		assert(col < _cols);
+		return _mat[row * _cols + col];
+	}
 
+	T &operator()(size_t row, size_t col) const
+	{
+		assert(row < _rows);
+		assert(col < _cols);
+		return _mat[row * _cols + col];
+	}
+
+	/** Stream operator
+		Overload for the operator<< for the matrix class
+	*/
 	friend std::ostream& operator<<(std::ostream& out, const matrix& m)
 	{
 		for (size_t r = 0; r < m._rows; ++r)
@@ -51,7 +92,6 @@ public:
 
 		return out;
 	}
-
 
 private:
 
