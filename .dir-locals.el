4d1898764116c5d400fe39d3b4fee1685c37fe29
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2025-01-17 01:00:45 +0100

    Config: Cmake update require to 3.10 (deprecation)
    .dir-locals.el: Not need special rule anymore for cuda headers.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
-((auto-mode-alist . (("/cuda/[^/]+\\.hpp\\'" . cuda-mode))) ;; cuda/*.hpp in cuda mode
+(;; (auto-mode-alist . (("/cuda/[^/]+\\.hpp\\'" . cuda-ts-mode)))
+ ;; cuda/*.hpp in cuda mode
  (nil . ((gtags-mode-update-args . "--gtagsconf=/usr/share/gtags/gtags.conf --gtagslabel=universal-ctags"))))
